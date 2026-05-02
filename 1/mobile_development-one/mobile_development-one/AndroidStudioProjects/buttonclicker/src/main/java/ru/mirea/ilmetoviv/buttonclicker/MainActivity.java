2aa977b5fbdddedcee419da55995b75841e05f58
Pr0grama <87766635+Pr0grama@users.noreply.github.com>
2026-05-02 12:11:15 +0300

    1 Practice

---
+++ 1/mobile_development-one/mobile_development-one/AndroidStudioProjects/buttonclicker/src/main/java/ru/mirea/ilmetoviv/buttonclicker/MainActivity.java
@@ -0,0 +1,49 @@
+package ru.mirea.ilmetoviv.buttonclicker;
+
+import android.os.Bundle;import android.view.View;import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView textViewStudent;
+    private Button btnWhoAmI;
+    private Button btnItIsNotMe;
+    private CheckBox checkBox;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        textViewStudent = findViewById(R.id.textViewStudent);
+        btnWhoAmI = findViewById(R.id.btnWhoAmI);
+        btnItIsNotMe = findViewById(R.id.btnItIsNotMe);
+        checkBox = findViewById(R.id.checkBox);
+        View.OnClickListener oclBtnWhoAmI = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textViewStudent.setText("Мой номер по списку № Х");
+                checkBox.setText("Мой номер по списку № Х");
+                checkBox.setChecked(true);
+            }
+        };
+
+        btnWhoAmI.setOnClickListener(oclBtnWhoAmI);
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+    }
+
+    public void onItsNotMeBtnClick(View view) {
+        textViewStudent.setText("Это не я сделал");
+        checkBox.setChecked(true);
+    }
+}
